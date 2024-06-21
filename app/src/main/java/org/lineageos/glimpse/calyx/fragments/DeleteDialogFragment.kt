d6b2e6169bc36b211cedb537bb4e1127152f17d3
Aayush Gupta <aayushgupta219@gmail.com>
2024-06-21 15:25:52 +0530

    DO NOT MERGE: Show delete confirmation dialog
    Change-Id: Ib474497e31713bfaf3823b4867313ff7c7e7e4c3
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

---
+++ app/src/main/java/org/lineageos/glimpse/calyx/fragments/DeleteDialogFragment.kt
@@ -0,0 +1,62 @@
+/*
+ * SPDX-FileCopyrightText: 2024 The Calyx Institute
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.glimpse.calyx.fragments
+
+import android.app.Dialog
+import android.content.DialogInterface
+import android.content.SharedPreferences
+import android.net.Uri
+import android.os.Bundle
+import android.widget.CheckBox
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
+import androidx.preference.PreferenceManager
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import org.lineageos.glimpse.R
+import org.lineageos.glimpse.calyx.ext.permanentlyDeleteFiles
+
+class DeleteDialogFragment private constructor(
+    private val quantity: Int,
+    private val positiveButtonClickListener: DialogInterface.OnClickListener
+) : DialogFragment() {
+
+    companion object {
+        private const val TAG = "DeleteDialogFragment"
+
+        fun show(
+            vararg uris: Uri,
+            manager: FragmentManager,
+            positiveButtonClickListener: DialogInterface.OnClickListener
+        ) {
+            DeleteDialogFragment(uris.size, positiveButtonClickListener)
+                .show(manager, TAG)
+        }
+    }
+
+    private lateinit var sharedPreferences: SharedPreferences
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val view = layoutInflater.inflate(R.layout.calyx_dialog_delete, null)
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext())
+
+        return MaterialAlertDialogBuilder(requireContext())
+            .setTitle(resources.getQuantityString(R.plurals.delete_confirmation_title, quantity))
+            .setView(view)
+            .setPositiveButton(getString(android.R.string.ok), positiveButtonClickListener)
+            .setNegativeButton(getString(android.R.string.cancel)) { _, _ -> dialog?.dismiss() }
+            .create()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        dialog?.findViewById<CheckBox>(R.id.checkBox)?.apply {
+            isChecked = sharedPreferences.permanentlyDeleteFiles
+            setOnCheckedChangeListener { _, isChecked ->
+                sharedPreferences.permanentlyDeleteFiles = isChecked
+            }
+        }
+    }
+}
